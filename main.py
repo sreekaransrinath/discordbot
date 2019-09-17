9eb122682a1e13c76413d973cb952ab4c812387d
skaranzx16 <50989977+PoseidonSigmaX@users.noreply.github.com>
2019-09-17 19:39:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = 'k.')
+client = commands.Bot(command_prefix = 'k.') #You can add any prefix here
 
-discord_token = input("Enter your discord token ")
+discord_token = input("Enter your discord token ") 
 
-@client.event
+#Checking if the script has started running successfully
+@client.event 
 async def on_ready():
     print("Ready to rumble")
 
+#Client Latency
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)} ms')
 
+#Purging Messages
 @client.command()
 async def clear(ctx, amount = 5):
     await ctx.channel.purge(limit = amount)
 
+#Kick
 @client.command()
 async def kick(ctx, member : discord.Member, *, reason = None):
     await member.kick(reason = reason)
     await ctx.send(f'Successfully kicked member')
 
+#Ban
 @client.command()
 async def ban(ctx, member : discord.Member, *, reason = None):
     await member.ban(reason = reason)
 
+#This function replies to a "Hello" message with a "Hello"
 @client.event
 async def on_message(message):
     if message.author == client.user:
